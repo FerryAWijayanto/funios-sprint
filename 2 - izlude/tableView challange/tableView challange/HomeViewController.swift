72a9dc867a5e49d8217585b195cdede4b30dda45
Gremlinflat <fahri.novaldi91100@gmail.com>
2022-08-13 15:52:00 +0700

    re-push

---
+++ 2 - izlude/tableView challange/tableView challange/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  tableView challange
+//
+//  Created by Fahri Novaldi on 12/08/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupView(){
+        let navBar = UIN
+    }
+
+}
+
---
+++ 2 - izlude/tableView challange/tableView challange/TaskModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskModel.swift
+//  tableView challange
+//
+//  Created by Fahri Novaldi on 13/08/22.
+//
+
+import Foundation
